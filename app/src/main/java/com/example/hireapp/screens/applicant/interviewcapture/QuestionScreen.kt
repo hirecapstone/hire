08d54c72a707c17d4f67d8670413b2dbe465fa60
KM4HS <ac4hsu@gmail.com>
2025-04-08 01:41:08 +0900

    fix: 하위 영상 저장 안 되던 문제 해결
    영상 저장 방식을 비동기 처리로 변경

--- app/src/main/java/com/example/hireapp/screens/applicant/interviewcapture/QuestionScreen.kt
+++ app/src/main/java/com/example/hireapp/screens/applicant/interviewcapture/QuestionScreen.kt
@@ -26,7 +26,12 @@ import com.example.hireapp.navigation.Screen
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import java.io.File
 
 @Composable
@@ -280,53 +285,53 @@ fun uploadVideoAndSave(
     val storageRef = storage.reference
     val VIDEO_PATH = "interview-films/${sessionId}"
 
-    val videoUrls = mutableListOf<String>()
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            // 저장을 다시 시도하는 경우 기존에 저장돼있던 영상들 삭제
+            if(reSaveFlag) {
+                storageRef.child(VIDEO_PATH).listAll().addOnSuccessListener { listResult ->
+                    listResult.items.forEach { it.delete() }
+                }
+            }
 
-    // 저장을 다시 시도하는 경우 기존에 저장돼있던 영상들 삭제
-    if(reSaveFlag) {
-        storageRef.child(VIDEO_PATH).listAll().addOnSuccessListener { listResult ->
-            listResult.items.forEach { it.delete() }
+            val videoUrls = mutableListOf<String>()
+
+            // storage에 업로드
+            recordedFiles.forEach { file ->
+                val fileUri = Uri.fromFile(file)
+                val videoRef = storageRef.child("${VIDEO_PATH}/${file.name}")
+
+                videoRef.putFile(fileUri).await()
+                val downloadUrl = videoRef.downloadUrl.await()
+                videoUrls.add(downloadUrl.toString())
+            }
+
+            // db에 저장
+            val videoData = hashMapOf(
+                "videoPath" to VIDEO_PATH,
+                "videos" to videoUrls
+            )
+
+            db.collection("videos")
+                .document(sessionId)
+                .set(videoData)
+                .await()
+
+            // 저장 완료되면 기존 영상들 삭제
+            withContext(Dispatchers.Main){
+                recordedFiles.forEach{ it.delete() }
+                navController.navigate(Screen.Capture.route) {
+                    popUpTo(Screen.Capture.route) { inclusive = true }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("UploadError", "업로드 실패: ${e.message}")
+            withContext(Dispatchers.Main) {
+                onError()
+            }
         }
+
     }
-
-    // storage에 업로드
-    recordedFiles.forEach { file ->
-        val fileUri = Uri.fromFile(file)
-        val videoRef = storageRef.child("${VIDEO_PATH}/${file.name}")
-
-        videoRef.putFile(fileUri).addOnSuccessListener {
-            videoRef.downloadUrl.addOnSuccessListener { uri ->
-                videoUrls.add(uri.toString())
-            }
-        }.addOnFailureListener {
-            Log.e("Storage", "영상 저장 실패: ${it.message}")
-            onError()
-        }
-    }
-
-    // db에 저장
-    // TODO: videos 배열은 전달되나, 저장되지 않는 문제 해결 필요
-    val videoData = hashMapOf(
-        "videoPath" to VIDEO_PATH,
-        "videos" to videoUrls.map { mapOf("fileUrl" to it) }
-    )
-
-    db.collection("videos")
-        .document(sessionId)
-        .set(videoData)
-        .addOnSuccessListener {
-            Log.d("Firestore", "URL 저장 성공")
-            for (recordedFile in recordedFiles) {
-                recordedFile.delete()
-            }
-            navController.navigate(Screen.Capture.route) {
-                popUpTo(Screen.Capture.route) { inclusive = true }
-            }
-        }
-        .addOnFailureListener {
-            Log.e("Firestore", "URL 저장 실패: ${it.message}")
-            onError()
-        }
 }
 
 @Composable
