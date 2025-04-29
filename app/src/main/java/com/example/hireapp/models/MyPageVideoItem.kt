4a2503ce744e933bd078d1f80d6609615454da92
KM4HS <ac4hsu@gmail.com>
2025-04-29 14:42:15 +0900

    feat: 마이페이지 인터뷰 목록 불러오기
    홈피드 필터 db에 맞게 구조 변경

--- app/src/main/java/com/example/hireapp/models/MyPageVideoItem.kt
+++ app/src/main/java/com/example/hireapp/models/MyPageVideoItem.kt
@@ -1,3 +1,10 @@
 package com.example.hireapp.models
 
-data class MyPageVideoItem()
+data class MyPageVideoItem(
+    val id: String = "",
+    val title: String = "",
+    val date: String = "",
+    val major: String = "",
+    val minor: String = "",
+    val isPublic: Boolean = false
+)
